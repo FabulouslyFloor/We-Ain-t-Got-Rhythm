bbe6b3b8533970ddd72af0ed402324b5e19eb3f5
FabulouslyFloor <113189357+FabulouslyFloor@users.noreply.github.com>
2022-10-07 00:14:48 -0400

    New Version

---
+++ Rhythm Game Overworld Test/MainCharacter.gd
@@ -0,0 +1,29 @@
+extends Sprite
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_key_pressed(KEY_UP):
+		self.position.y -= 5
+	if Input.is_key_pressed(KEY_DOWN):
+		self.position.y += 5
+	if Input.is_key_pressed(KEY_LEFT):
+		self.position.x -= 5
+		self.scale.x = 0.197
+	if Input.is_key_pressed(KEY_RIGHT):
+		self.position.x += 5
+		self.scale.x = -0.197
+
+
+func _on_Area2D_body_entered(body):
+	pass # Replace with function body.
