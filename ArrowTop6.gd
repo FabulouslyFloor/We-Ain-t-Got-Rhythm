856771c499304cf67108a4972901fe0a979c4ebb
SidKaeb <112952128+SidKaeb@users.noreply.github.com>
2022-10-14 08:36:52 -0400

    Added Arrow Files

---
+++ ArrowTop6.gd
@@ -0,0 +1,44 @@
+extends Sprite
+
+#var timer
+var time_start = 0
+var time_now = 0
+var score = 0
+
+var A = 1269
+var B = 5000
+var C = 5500
+
+func _ready():
+	time_start = OS.get_ticks_msec()
+	
+	# Set the position of Sprite to the center of screen
+	position = Vector2(get_viewport().size.x, get_viewport().size.y/3)
+	#Scale x and y coordinate to 30% of what they currently are
+	scale = Vector2(0.17,0.17)
+	set_process(true) #calls process function
+	
+func _process(delta):
+	time_now = OS.get_ticks_msec()
+	var time_elapsed = time_now - time_start
+	#move sprite to the left
+	if time_elapsed < A:
+		self.hide()
+	if time_elapsed >= A:
+		translate(Vector2(-100 * delta*2,0))
+	if time_elapsed < B && time_elapsed > A:
+		self.show()	
+	
+	#if sprite reaches left side, hide it
+	if(position.x < get_viewport().size.x/9):
+		self.hide()
+	#Wait until in the clickable area for score
+	if(position.x < get_viewport().size.x/3):
+		if time_elapsed >= B && time_elapsed < C:
+			if Input.is_key_pressed(KEY_UP):
+				score += 1
+				self.hide()
+				
+				
+	if Input.is_key_pressed(KEY_Q):
+		get_tree().quit()
