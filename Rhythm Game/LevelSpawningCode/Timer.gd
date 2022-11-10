b0eb5037812f8916a1819651f2783ae286d51c79
SidKaeb <112952128+SidKaeb@users.noreply.github.com>
2022-11-09 21:08:15 -0500

    Add files via upload

--- Rhythm Game/LevelSpawningCode/Timer.gd
+++ Rhythm Game/LevelSpawningCode/Timer.gd
@@ -1,37 +1,42 @@
 extends Timer
 
-var ArrowTop = preload("res://LevelSpawningCode/ArrowTopScene.tscn")
-var ArrowMiddle = preload("res://LevelSpawningCode/ArrowMiddleScene.tscn")
-var ArrowBottom = preload("res://LevelSpawningCode/ArrowBottomScene.tscn")
+var time_start = 0
+var time_now = 0
+var score = 0
+var ArrowTop = preload("res://ArrowTopScene.tscn")
+var ArrowMiddle = preload("res://ArrowMiddleScene.tscn")
+var ArrowBottom = preload("res://ArrowBottomScene.tscn")
 
 func _ready():
-	#wait_time = rand_range(0,1)
-	#print("UR MOM")
-	wait_time = 2
-	start()
+	time_start = OS.get_ticks_msec()
 	set_process(true)
 
 func _process(delta):
-	print(get_time_left())
+	time_now = OS.get_ticks_msec()
+	var time_elapsed = time_now - time_start
 	
+	if Input.is_key_pressed(KEY_Q):
+		get_tree().quit()
+		
+	print(get_time_left())
 
-func _on_Timer_timeout():
-	print("UR MOM")
 	var arrows = [ArrowTop, ArrowMiddle, ArrowBottom]
 	var kinds = arrows[randi()% arrows.size()]
 	var arrow = kinds.instance()
-	
-	#fix this
-	if arrow == ArrowTop:
+	if time_elapsed >= 1:
+		arrow = arrows[0].instance()
 		arrow.position = Vector2(get_viewport().size.x, get_viewport().size.y*2/3)
 		arrow.scale = Vector2(0.17,0.17)
-		print("Arrow Top")
-	elif arrow == ArrowMiddle:
-		arrow.position = Vector2(get_viewport().size.x, get_viewport().size.y/2)
-		arrow.scale = Vector2(0.17,0.17)
-	elif arrow == ArrowBottom:
-		arrow.position = Vector2(get_viewport().size.x, get_viewport().size.y/3)
-		arrow.scale = Vector2(0.17,0.17)		
-	#arrow.translate(Vector2(-100 * delta*1.8,0))
+		arrow.translate(Vector2(-100 * delta*1.8,0))
+		
+		if(arrow.position.x < get_viewport().size.x/9):
+			arrow.queue_free
+		if(arrow.position.x < get_viewport().size.x/3):
+			if Input.is_key_pressed(KEY_LEFT):
+				score += 1
+				arrow.queue_free()
+		
+	
+
 	add_child(arrow)
 	start()
