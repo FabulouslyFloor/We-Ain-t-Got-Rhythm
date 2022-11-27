d334ba502b0d8709eaf8813546c5545eb85b0313
[sidKaeb] <[skaeb@muhlenberg.edu]>
2022-11-26 19:22:56 -0500

    new

--- Rhythm Game/LevelSpawningCode/MainScene.gd
+++ Rhythm Game/LevelSpawningCode/MainScene.gd
@@ -6,13 +6,12 @@ var ArrowTopOpen = preload("res://LevelSpawningCode/ArrowOpenTopScene.tscn")
 var ArrowBottomOpen = preload("res://LevelSpawningCode/ArrowOpenBottomScene.tscn")
 var ArrowMiddleOpen = preload("res://LevelSpawningCode/ArrowOpenMiddleScene.tscn")
 #var ArrowTopOpen = preload("res://LevelSpawningCode/ArrowTopScene.tscn")
-
+var player = AudioStreamPlayer.new()
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_process(true)
-	var player = AudioStreamPlayer.new()
 	self.add_child(player)
 	player.stream = load("res://LevelSpawningCode/1-13 Wait For It.mp3")
 	player.play()
@@ -41,8 +40,5 @@ func _process(_delta):
 	var time_now = OS.get_ticks_msec()
 	var time_elapsed = time_now - time_start
 	
-#	if time_elapsed > 3000 && time_elapsed < 3020:
-#		var player = AudioStreamPlayer.new()
-#		self.add_child(player)
-#		player.stream = load("res://LevelSpawningCode/1-13 Wait For It.mp3")
-#		player.play()
+	if time_elapsed > 158140:
+		player.stop()
